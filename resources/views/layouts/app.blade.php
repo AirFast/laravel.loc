26e5f7385279c9b1f9942cefa1e1414aa9601d25
AirFast <airfast.88@gmail.com>
2020-11-12 21:32:50 +0200

    Users controller

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,7 @@
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav ml-auto">
+                <ul class="navbar-nav ml-auto d-flex flex-row justify-content-center">
 
                     @foreach(config('app.locales') as $locale => $lang)
                         <li class="nav-item mx-1 my-2">
