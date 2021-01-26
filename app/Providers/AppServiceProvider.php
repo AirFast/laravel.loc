936722a184a36bb4288897d3790b5fab1f78dfc9
AirFast <airfast.88@gmail.com>
2021-01-26 09:00:09 +0200

    adding user functionality

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,8 +26,8 @@ class AppServiceProvider extends ServiceProvider {
     public function boot() {
 
         if ( env( 'APP_ENV' ) !== 'local' ) {
-            URL::forceScheme( 'https' );
-            $this->app['request']->server->set('HTTPS', true);
+            //URL::forceScheme( 'https' );
+            $this->app['request']->server->set('https', true);
         }
 
         if ( Schema::hasTable( 'settings' ) ) {
