a745a641c932702e1882dd6888a0aeded44f1c41
AirFast <airfast.88@gmail.com>
2021-02-13 16:06:45 +0200

    app bg

--- resources/views/emails/territory.blade.php
+++ resources/views/emails/territory.blade.php
@@ -3,9 +3,9 @@
 <h1 style="text-align: center">{{ __('emails.territory.title') }}</h1>
 <h1>{{ __('emails.greeting') }}</h1>
 
-<p>{{ __('emails.territory.text', ['name' => $data['name'], 'number' => $data['number'], 'address' => $data['address']]) }}</p>
+<p>{{ __('emails.territory.text', ['name' => $mailData['name'], 'number' => $mailData['number'], 'address' => $mailData['address']]) }}</p>
 
-@component('mail::button', ['url' => $data['url']])
+@component('mail::button', ['url' => $mailData['url']])
     {{ __('emails.territory.btn') }}
 @endcomponent
 
@@ -13,6 +13,6 @@
 <hr style="border:none; height: 1px; background: #edf2f7">
 <br>
 
-<p><small>{{ __('emails.info', ['url' => $data['url']]) }}</small></p>
+<p><small>{{ __('emails.info', ['url' => $mailData['url']]) }}</small></p>
 
 @endcomponent
